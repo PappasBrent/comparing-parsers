484a98219573bde5265ab6af4c7c9c91782d3089
Brent Pappas <pappasbrent@gmail.com>
2022-07-18 16:10:21 -0400

    finish setting up evaluation script

--- run_evaluation.py
+++ run_evaluation.py
@@ -4,7 +4,7 @@ import filecmp
 import os
 import subprocess
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 @dataclass
@@ -28,6 +28,15 @@ PARSERS = [
 ]
 
 
+def time_parser_on_input(p: Parser, fn: str) -> timedelta:
+    start = datetime.now()
+    subprocess.run(
+        f'cat ../{fn} | {p.run_script} > ../results/{p.dir}.txt',
+        shell=True)
+    end = datetime.now()
+    return end - start
+
+
 def main():
     # build the parsers
     for p in PARSERS:
@@ -35,33 +44,38 @@ def main():
         subprocess.run(p.build_script, shell=True, capture_output=True)
         os.chdir('..')
 
-    ifns = [os.path.join('inputs/', ifn)
-            for ifn in os.listdir('inputs/')]
+    # collect input files
+    fns = [os.path.join('inputs/', fn) for fn in os.listdir('inputs/')]
 
-    # run the programs
-    for ifn in ifns:
-        print(f'# {ifn}')
+    # for each input
+    for fn in fns:
+        print(f'# {fn}')
 
+        # for each parser
         for p in PARSERS:
+            # change into the parser's directory
             os.chdir(p.dir)
 
-            start = datetime.now()
-            subprocess.run(
-                f'cat ../{ifn} | {p.run_script} > ../results/{p.dir}.txt',
-                shell=True)
-            end = datetime.now()
-            elapsed = end - start
-            print(f'    {p.dir}: {elapsed.seconds}.{elapsed.microseconds}')
+            # run the parser twice to warm up the cache
+            time_parser_on_input(p, fn)
+            time_parser_on_input(p, fn)
 
+            # run the parser 5 times, and take the average of those times
+            # averaging timedeltas: https://stackoverflow.com/a/3617540/6824430
+            times = [time_parser_on_input(p, fn) for _ in range(5)]
+            avg = sum(times, timedelta(0)) / len(times)
+            print(f'    {p.dir}: {avg.seconds}.{avg.microseconds}')
+
+            # go back to the top-level directory
             os.chdir('..')
 
-        # check that all files are the same
+        # check that output of all parsers is the same
         results = os.listdir('results')
         os.chdir('results')
         for r1 in results:
             for r2 in [r for r in results if r != r1]:
                 if not filecmp.cmp(r1, r2):
-                    print(r1, r2)
+                    print(f'{r1} != {r2}')
 
 
 if __name__ == '__main__':
